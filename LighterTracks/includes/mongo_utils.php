f1f0bc14f9c219df17a86f01c86a04a476da91e7
Chadwick Puterbaugh <puterbaugh.cw@gmail.com>
2014-10-30 13:01:49 -0400

    Further development on mongo_utils, changed name of index.html to .php in anticipation of connection

--- LighterTracks/includes/mongo_utils.php
+++ LighterTracks/includes/mongo_utils.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 class lighter{
 
@@ -6,43 +8,65 @@ class lighter{
     private $port    = 27017;
     private $c       = null; // collection
     private $id      = null; // lighter id
+    private $doc     = null;
 
-    public function __construct($addy, $pt){
+    public function __construct($addy, $pt, $id){
         $this->address = $addy;
         $this->port = $pt;
-        $this->c = new MongoClient($this->address, $this->port)->LighterTrackers->lighters;
+        # $client = new MongoClient('mongodb://192.168.1.120:27017');
+        $client = new MongoClient();
+        $this->c = $client->LighterTracker->lighters;
+        $this->id = $id;
 
         // search if lighter exists, if not, create
-        // to-do 
-    }
-        
-    private function insert_self(){
-        // insert blank self into db;
+        $result = $this->find_self($id);
+        if (!$result->count()){
+            echo 'instantiating';
+            $this->insert_self($id); 
+            $this->doc = $this->find_self($id)->getNext();
+        }else{
+            $this->doc = $result->getNext();
+            # echo 'found ' . $this->doc['id'];
+        }
     }
 
-    public function update_location(){
+    private function find_self($id){
+        return $this->c->find(array('id' => $id));
+    }
+ 
+    private function insert_self($id){
+        // insert blank self into db;
+        $this->c->insert(array('id' => $id));
+    }
+
+    private function update(){
+        //pass
+        $this->c->modify(array('id' => $this->doc['id']), $this->doc);
+    }
+    public function update_location($location){
         // append a new lat/long into lighter
         // to do 
+        array_push($this->doc['locations'], $this->validate_location($location));
+        $this->update();
     }
 
     public function update_user(){
         // append a new user id into lighter
         // to do
-        // create new user? 
+        array_push($this->doc['users'], user);
+        $this->update();
     }
 
     public function get_past_locs(){
         // return array of past locations
         // to do
-        $locs = [];
-        return $locs;
+        return $this->doc['locations'];
     }
 
     public function get_past_users(){
         // return array of past users
         // to do 
-        $users = [];
-        return $users[];
+        return $this->doc['users'];
     }
 
     public function alter_past_loc($num, $new_loc){
@@ -58,11 +82,69 @@ class lighter{
     static function validate_location($location){
         // takes in a lat/long lcation and validates form
         // to do
-        return $location
+        $location = split('/', $location);
+        $lat = floatval($location[0]);
+        $long= floatval($location[1]);
+        if( $lat and $long ){
+            if ($lat[0] >= -90 and $lat[0] <= 90 and $long[1] >= -180 and $long[1] <= 180 ){
+                return $location;
+            }
+        }else{
+            echo "Lat/long wrong format";
+        }
     }
-
-
 
 }
 
+$a = new lighter('localhost', 27017, 1);
+
+
+class Db_relate{
+
+    private $port = 27017;
+    private $address = 'localhost';
+    private $db = null;
+
+    public function __construct($addy, $port){
+        $this->port = $port;
+        $this->address = $addy;
+        # $mc= new MongoClient('mongodb://192.168.1.120:27017');
+        $mc= new MongoClient();
+        $this->db = $mc->LighterTracker;
+    }
+
+    public function get_all_lighters($registered = True){
+        $c = $this->db->lighers;
+        if($registered){
+            $pipe = null;
+        }else{
+            $pipe = null;
+        }
+        $result = $c->aggregate($pipe); 
+        return $result;
+    }
+
+    public function get_all_users(){
+        return null;
+    }
+
+    public function get_all_locations(){
+        $c = $this->db->lighters;
+        $pipe = array(
+            '$match' => array(
+                'locations' => 
+                    array('$exists' => 'true')
+                )
+        );
+        $result = $c->aggregate($pipe);
+        return $result;
+    }
+}
+
+$b = new Db_relate('localhost', 27017);
+$result = $b->get_all_locations();
+foreach($result as $doc){
+    var_dump($doc);
+}
+echo 'it works';
 ?>
